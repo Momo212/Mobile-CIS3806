4a64de4ab89ced7908dbd4299fe5b9d4080870ce
bokklu <danzanzu@gmail.com>
2017-12-20 17:08:51 +0100

    Added dashboard and toolbar.

---
+++ App1/App1/Search.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Search : ContentPage
+    {
+        public Search()
+        {
+            InitializeComponent();
+
+            var settings = new ToolbarItem
+            {
+                Icon = "Assets/home.png",
+                Text = "Dashboard",
+                Order = ToolbarItemOrder.Secondary,
+                Command = new Command(this.ShowDashboard),
+            };
+            
+            this.ToolbarItems.Add(settings);
+        }
+
+        private void ShowDashboard()
+        {
+            this.Navigation.PushAsync(new MainPage());
+        }
+    }
+}
